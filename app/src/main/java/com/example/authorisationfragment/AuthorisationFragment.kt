abfc9c954940a5e629aa7f3926bb93893f56f6f1
AndroidMaster <ya-mihim@yandex.ru>
2025-02-25 22:35:19 +0300

    task_ready

---
+++ app/src/main/java/com/example/authorisationfragment/AuthorisationFragment.kt
@@ -0,0 +1,35 @@
+package com.example.authorisationfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.authorisationfragment.databinding.FragmentAuthorisationBinding
+
+class AuthorisationFragment : Fragment() {
+
+    private var _binding: FragmentAuthorisationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthorisationBinding.inflate(inflater, container, false)
+        setupListeners()
+        return binding.root
+    }
+
+    private fun setupListeners() {
+        binding.registration.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment_container_view, RegistrationFragment())
+                ?.addToBackStack("registration")?.commit()
+        }
+        binding.login.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fragment_container_view, MainScreenFragment())?.commit()
+        }
+    }
+}
